197a18dd6accd8da3df7eb7c24f77b1769a88b6a
HOÀNG VĂN SÂM - ITC - Data Science <sam.hoang@mservice.com.vn>
2021-06-18 11:14:46 +0700

    fix install pyls ms in linux

--- script/install-pyls-ms.sh
+++ script/install-pyls-ms.sh
@@ -8,13 +8,14 @@ set -e
 # os=$(uname -s | tr "[:upper:]" "[:lower:]")
 # case $os in
 #     linux)
-#         curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- -i "./.dotnet"
+#         curl -L https://dot.net/v1/dotnet-install.sh | bash -s --  --verbose --install-dir "$HOME/.local/.dotnet"
 #         ;;
 #     darwin)
 #         brew install --cask dotnet
 #         ;;
 #     *) ;;
 # esac
+# ln -s "$HOME/.local/.dotnet/dotnet" "$HOME/.local/bin"
 
 os=$(uname -s | tr "[:upper:]" "[:lower:]")
 case $os in
@@ -27,15 +28,18 @@ case $os in
     *) ;;
 esac
 
+lsp_path="$HOME/.local/share/nvim/python-ms-lsp"
+lsp_path_data="$lsp_path/data"
+mkdir -p "$lsp_path"
+
 version="0.5.59"
 url="https://pvsc.azureedge.net/python-language-server-stable/Python-Language-Server-${system}-x64.${version}.nupkg"
 
 nupkg="./pyls.nupkg"
-lsp_path="$HOME/.local/share/nvim/python-ms-lsp"
-lsp_path_data="$lsp_path/data"
-mkdir -p "$lsp_path"
+
 curl -L "$url" -o "$nupkg"
 unzip "$nupkg" -d  "$lsp_path_data"
+rm "$nupkg"
 
 cat <<EOF >pyls-ms
 #!/usr/bin/env bash
@@ -45,6 +49,5 @@ EOF
 
 chmod +x pyls-ms
 mv pyls-ms "$lsp_path"
-rm "$nupkg"
 
 
