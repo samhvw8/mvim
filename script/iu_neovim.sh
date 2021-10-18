203e6d1559ad8bada5a80a3ca76e6a8278ab65bd
sam hoang <samhv.ict@gmail.com>
2021-10-19 03:28:55 +0700

    update

--- script/iu_neovim.sh
+++ script/iu_neovim.sh
@@ -1,4 +1,4 @@
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-chmod u+x nvim.appimage
+sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin
 sudo ln -snf /usr/local/bin/nvim.appimage /usr/local/bin/nvim
